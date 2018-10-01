549445fb0dabba59ad1c116ff5becac6c513e739
William Fergus <Will.Fergus@iam.local>
2018-10-01 15:16:38 -0500

    adds progress on playercharacter script

---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour {
+
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private Rigidbody2D myRigidbody;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.D))
+        {
+            myRigidbody.velocity = new Vector2(speed, myRigidbody.velocity.y);
+        }
+    }
+
+}
